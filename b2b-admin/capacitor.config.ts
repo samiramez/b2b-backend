311860e9c2ee9f342a98225847857c6d5302189c
سامي اطنه لي <AD1008@iu.edu.jo>
2025-08-15 22:03:08 +0300

    ......

--- b2b-admin/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'b2b-admin',
-  webDir: 'www'
-};
-
-export default config;
